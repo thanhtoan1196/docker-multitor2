d1336d3964935128fa2c67a8e5a454dfe698d300
toantran <thanhtoan1196@gmail.com>
2021-10-30 11:16:37 +0700

    change proxy

--- startup.sh
+++ startup.sh
@@ -14,4 +14,4 @@ if ! [[ $TOR_INSTANCE_COUNT =~ $re ]] ; then
 fi
 
 
-multitor --init $TOR_INSTANCE_COUNT --user root --socks-port 9000 --control-port 9900 --proxy privoxy --verbose --debug > /var/log/multitor.log; tail -f /var/log/multitor.log
+multitor --init $TOR_INSTANCE_COUNT --user root --socks-port 9000 --control-port 9900 --proxy privoxy --haproxy --verbose --debug > /var/log/multitor.log; tail -f /var/log/multitor.log
